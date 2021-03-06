c3df6e5c50b6c4b5f2c6c3c771cc2277c500aba4
lordanubi <lordanubi@icloud.com>
2021-03-06 18:36:44 +0100

    updated pattern maker

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import * as Const from './Const'
-import Atom from './Atom/Atom'
-import PatternView from './views/PatternView/PatternView';
+import AtomDef from './Atom/Def'
+import PixelGridDef from './Patterns/PixelGrid/Def'
+import PatternView from './Views/PatternView/PatternView';
 function App() {
+  //in questa parte dell'App verranno caricati tutti i componenti svg che possono essere cachati
   return (
     <>
-      <svg className="symbol">
-        <symbol id="Atom" viewBox={"0 0 " + Const.logoWidth + " " + Const.logoHeight}>
-            <Atom />
-        </symbol>
+      <svg className="symbols">
+            <AtomDef/>
+            <PixelGridDef />
+            
       </svg>
       <PatternView />
     </>
--- src/views/PatternView/PatternView.js
+++ src/views/PatternView/PatternView.js
@@ -1,45 +1,20 @@
 import React from 'react';
 import './PatternView.css';
 import * as Const from '../../Const'
-import PatternMaker from '../../Atlantide/Columns/PatternMaker'
-import {motion} from 'framer-motion'
+import GridMaker from '../../Atlantide/GridMaker'
+import PatternEditor from '../../Components/PatternEditor';
+
 function PatternView(props)  {
-  const gradientVariants = {start: {x1: '1%'}, end: {x1: '99%'}}
-  const transition = {
-    type: 'spring',
-    duration: 2,
-    bounce: 0.1,
-    repeat: Infinity,
-    repeatType: 'reverse'
-  }
-  let patternSettings = {
-    horizontal: {
-      spacing: 0,
-      atoms: 20,
-      quasiPeriodicHorizontalFlip: true,
-      evenOddVerticalFlip: true,
-      phase: false
-    },
-    vertical: {
-      spacing: 0,
-      atoms: 53,
-      quasiPeriodicHorizontalFlip: true,
-      evenOddVerticalFlip: true,
-      phase: false
-    },
-    withCouples: false,
-    chaotic: false
-  }
+  
   return(
-    <svg viewBox={"0 0 " + Const.logoWidth + " " + Const.logoHeight} className="svgViewer">
-      <defs>
-              <motion.linearGradient initial="start" animate="end" transition={transition} variants={gradientVariants} id="glow" x1="0%" y1="0%" x2="100%" y2="0%">
-                <stop offset="0%"  stop-color="rgba(0, 0, 0, 0)"/>
-                <stop offset="100%" class="glowColor" />
-              </motion.linearGradient>
-      </defs>
-      <PatternMaker set={patternSettings} />
+    <>
+    <svg preserveAspectRatio="xMidYMid slice" viewBox="0 0 100 100" width="100%" height="100%" className="svgViewer">
+          <use href="#PixelGrid" />
     </svg>
+    <svg className="interface" viewBox="0 0 100 100" width="100%" height="100%" >
+      <circle cx="50" cy="50" r="50" />
+    </svg>
+    </>
   )
 }
 export default PatternView;
